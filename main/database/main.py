77cdd4fc4caf52fab4f474b4ac22bcbc88d560c6
Pawiu__ <pawelkoc681@gmail.com>
2023-09-25 11:27:24 +0200

    0.1.4

--- main/database/main.py
+++ main/database/main.py
@@ -1,1 +1,27 @@
-#TODO: database connection
+#TODO: database connection, import config file
+
+import pymongo
+import sys
+import os
+
+# Get the parent directory of the current script (myscript.py)
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Add the parent directory (/main/) to the Python path
+sys.path.append(parent_dir)
+from .. import config
+database =config.database
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+
+mydb = myclient[database.name]
+
+dblist = myclient.list_database_names()
+if "SIAAI" in dblist:
+  print("The database exists.")
+
+mycol = mydb[database.collections.request]
+
+collist = mydb.list_collection_names()
+if "request" in collist:
+  print("The collection exists.")
